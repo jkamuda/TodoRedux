bd15ddd4cfe8cddfafe852061957c9db6b1daba9
Jason Kamuda <jkamuda@gmail.com>
2016-03-04 23:55:52 -0500

    pretty simple form up and running, also using React.createClass now

--- static/js/components/App.js
+++ static/js/components/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-  }
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      items: [],
+      text: ''
+    };
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const {text} = this.state;
+    console.log('submit: ' + text);
+  },
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  },
 
   render() {
     return (
       <div>
       	Hello world!
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #'}</button>
+        </form>
       </div>
     );
   }
-}
+});
