62635b79a18c650a9f40e36c45e555475dbaeedb
Jason Kamuda <jkamuda@gmail.com>
2016-03-13 21:05:24 -0400

    cleaned up a few redux issues

--- static/js/components/App.js
+++ static/js/components/App.js
@@ -16,7 +16,6 @@ const App = React.createClass({
   handleSubmit(e) {
     e.preventDefault();
     const {text} = this.state;
-    console.log('submit: ' + text);
     this.props.onTodoClick(text);
     this.setState({text: ''});
   },
@@ -25,21 +24,30 @@ const App = React.createClass({
     this.setState({text: e.target.value});
   },
 
+  renderList() {
+    const {todos} = this.props;
+    return todos.map(todo =>
+      <li key={todo.id}>{todo.text}</li>
+    );
+  },
+
   render() {
     return (
       <div>
-      	Hello world!
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #'}</button>
+          <button>{'Add Todo'}</button>
         </form>
+        <ul>
+          {this.renderList()}
+        </ul>
       </div>
     );
   }
 });
 
 const mapStateToProps = (state) => {
-  return { todos: state.todos }
+  return { todos: state }
 };
 
 const mapDispatchToProps = (dispatch) => {
--- static/js/reducers/index.js
+++ static/js/reducers/index.js
@@ -1,7 +1,17 @@
 
 const todo = (state = [], action) => {
   console.log('handling action: ' + action.type + " " + action.text);
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          text: action.text
+        }
+      ];
+    default:
+      return state;
+  }
 }
 
 export default todo;
